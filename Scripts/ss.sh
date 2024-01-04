6fa5c25df5101410bb355724fd6591a1e619fbe3
ksnxyz <123903734+ksnxyz@users.noreply.github.com>
2024-01-04 20:55:29 +0100

    Update ss.sh

--- Scripts/ss.sh
+++ Scripts/ss.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Sökväg till mappen där skärmbilderna ska sparas
-screenshot_folder="$HOME/Pictures/Screenshots"
+screenshot_folder="$HOME/Documents/Pictures/Screenshots"
 
 # Filnamnet för skärmbilden baserat på datum och tid
 screenshot_file="$screenshot_folder/screenshot_$(date "+%Y%m%d_%H%M%S").png"
 
 if [[ $1 == "select" ]]; then
     # Ta en skärmbild av ett markerat område
-    maim -s -u -l -c 1,0,1.5,0.2 -b 2 | tee "$screenshot_file" | xclip -selection clipboard -t image/png
+    maim -s -u -l -c 1,0,1.5,0.2 -b 2 -m 10 | tee "$screenshot_file" | xclip -selection clipboard -t image/png
 else
     # Ta en skärmbild av hela skärmen
     maim -u | tee "$screenshot_file" | xclip -selection clipboard -t image/png
